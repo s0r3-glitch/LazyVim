2432e45094e62d5450881ae47c87891c6a81c4d3
geygo9 <geoffrey.goodson@lfg.com>
2024-06-27 16:33:24 -0600

    updated the dashboard to open nvimtree

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -350,6 +350,7 @@ return {
             local stats = require("lazy").stats()
             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
             return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+            require("nvim-tree.api").tree.open()
           end,
         },
       }
